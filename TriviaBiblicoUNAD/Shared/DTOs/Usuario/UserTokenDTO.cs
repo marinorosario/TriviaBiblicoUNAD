4812b57be384024ec0401ebae92e1ed259c63dab
marino.rosario <marino.rosario@map.gob.do>
2023-11-05 11:56:13 -0400

    Sistema de Usuario y Autenticacion, Tablas, API, y Manejador de Eventos de autenticacion de prueba en el FrontEnd

---
+++ TriviaBiblicoUNAD/Shared/DTOs/Usuario/UserTokenDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaBiblicoUNAD.Shared.DTOs.Usuario
+{
+    public class UserTokenDTO
+    {
+        public UserTokenDTO(string token, DateTime expiration)
+        {
+            Token = token;
+            Expiration = expiration;
+        }
+
+        public string Token { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+    }
+}
